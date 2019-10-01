5c9b17de11bcfb9c2ce99a12eb6f87ad74d70c0c
otymko <olegtymko@yandex.ru>
2019-10-01 10:33:23 +0700

    permission for gradlew

---
+++ src/main/java/org/github/otymko/phoenixbsl/views/IssuesDialog.java
@@ -0,0 +1,16 @@
+package org.github.otymko.phoenixbsl.views;
+
+import javax.swing.*;
+
+public class IssuesDialog extends JDialog {
+
+  public IssuesDialog() {
+
+    setBounds(100,100,500,600);
+    toFront();
+    setVisible(true);
+    setAlwaysOnTop(true);
+
+  }
+
+}
---
+++ src/test/java/org/github/otymko/phoenixbsl/IssuesDialogTest.java
@@ -0,0 +1,16 @@
+package org.github.otymko.phoenixbsl;
+
+import org.github.otymko.phoenixbsl.views.IssuesDialog;
+import org.junit.Test;
+
+public class IssuesDialogTest {
+
+  @Test
+  public void testForm() {
+
+    IssuesDialog dialog = new IssuesDialog();
+
+
+  }
+
+}
