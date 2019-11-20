c606e2b0222574475596d4b88c97a36a95a5fbdf
Tymko Oleg <olegtymko@yandex.ru>
2019-11-20 20:36:36 +0700

    чистка

--- src/main/java/org/github/otymko/phoenixbsl/Launcher.java
+++ src/main/java/org/github/otymko/phoenixbsl/Launcher.java
@@ -8,16 +8,6 @@ public class Launcher {
   private static final Logger log = LoggerFactory.getLogger(Launcher.class);
 
   public static void main(String[] args) {
-
-    App app = App.getInstance();
-    if (app.appIsRunning()) {
-      app.abort();
-    }
-    try {
-      app.run();
-    } catch (RuntimeException ex) {
-      log.error("Приложение упало. Причина " + ex.getMessage());
-    }
   }
 
 }
