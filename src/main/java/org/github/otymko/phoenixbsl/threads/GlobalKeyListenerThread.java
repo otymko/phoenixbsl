6a386e27ba0246fe734aecc0ddefef3fa236ad8d
Tymko Oleg <olegtymko@yandex.ru>
2020-02-08 11:15:34 +0700

    подготовка к настройке сочетания клавиш + добавлена пауза

--- src/main/java/org/github/otymko/phoenixbsl/threads/GlobalKeyListenerThread.java
+++ src/main/java/org/github/otymko/phoenixbsl/threads/GlobalKeyListenerThread.java
@@ -5,19 +5,41 @@ import lc.kra.system.keyboard.event.GlobalKeyAdapter;
 import lc.kra.system.keyboard.event.GlobalKeyEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.github.otymko.phoenixbsl.core.PhoenixApp;
+import org.github.otymko.phoenixbsl.entities.KeyboardShortcut;
 import org.github.otymko.phoenixbsl.events.EventManager;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
 public class GlobalKeyListenerThread extends Thread {
+
+  private PhoenixApp app = PhoenixApp.getInstance();
+  private Map<KeyboardShortcut, Runnable> commands = new HashMap<>();
+
+  public GlobalKeyListenerThread() {
+
+    commands.put(
+      new KeyboardShortcut(GlobalKeyEvent.VK_I, true),
+      () -> app.getEventManager().notify(EventManager.EVENT_INSPECTION));
+
+    commands.put(
+      new KeyboardShortcut(GlobalKeyEvent.VK_K, true),
+      () -> app.getEventManager().notify(EventManager.EVENT_FORMATTING));
+
+    commands.put(
+      new KeyboardShortcut(GlobalKeyEvent.VK_J, true),
+      () -> app.getEventManager().notify(EventManager.EVENT_FIX_ALL));
+
+  }
 
   @Override
   public void run() {
 
     try {
       runHook();
-    } catch (Exception e) {
+    } catch (IllegalStateException e) {
+      LOGGER.error("Ошибка", e);
       run();
     }
 
@@ -27,27 +49,30 @@ public class GlobalKeyListenerThread extends Thread {
 
     GlobalKeyboardHook keyboardHook = new GlobalKeyboardHook(false);
     LOGGER.info("Global keyboard hook successfully started. Connected keyboards:");
-    for (Map.Entry<Long, String> keyboard : GlobalKeyboardHook.listKeyboards().entrySet()) {
-      LOGGER.info("{}: {}\n", keyboard.getKey(), keyboard.getValue());
-    }
 
     keyboardHook.addKeyListener(new GlobalKeyAdapter() {
-
       @Override
       public void keyPressed(GlobalKeyEvent event) {
+        var isSupportAction = false;
         if (event.isControlPressed()) {
-          if (event.getVirtualKeyCode() == GlobalKeyEvent.VK_I) {
-            PhoenixApp.getInstance().getEventManager().notify(EventManager.EVENT_INSPECTION);
+
+          var keyShortcut = new KeyboardShortcut(event.getVirtualKeyCode(), true);
+          Runnable method = commands.entrySet().stream()
+            .filter(pair -> pair.getKey().equals(keyShortcut))
+            .map(Map.Entry::getValue)
+            .findFirst()
+            .orElse(null);
+
+          if (method != null) {
+            isSupportAction = true;
+            method.run();
           }
-          if (event.getVirtualKeyCode() == GlobalKeyEvent.VK_K) {
-            PhoenixApp.getInstance().getEventManager().notify(EventManager.EVENT_FORMATTING);
-          }
-          if (event.getVirtualKeyCode() == GlobalKeyEvent.VK_J) {
-            PhoenixApp.getInstance().getEventManager().notify(EventManager.EVENT_FIX_ALL);
-          }
+
+        }
+        if (isSupportAction) {
+          PhoenixApp.getInstance().sleepCurrentThread(50);
         }
       }
-
     });
 
   }
