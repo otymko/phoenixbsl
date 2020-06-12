0512abe35fad03a0ce76241fde69cb12d1f24749
Tymko Oleg <olegtymko@yandex.ru>
2020-06-12 11:35:27 +0700

    lombok для issue

--- src/main/java/com/github/otymko/phoenixbsl/model/Issue.java
+++ src/main/java/com/github/otymko/phoenixbsl/model/Issue.java
@@ -1,8 +1,12 @@
 package com.github.otymko.phoenixbsl.model;
 
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import lombok.Getter;
+import lombok.Setter;
 import org.eclipse.lsp4j.DiagnosticSeverity;
 
+@Getter
+@Setter
 public class Issue extends RecursiveTreeObject<Issue> {
 
   private String description = "";
@@ -10,40 +14,9 @@ public class Issue extends RecursiveTreeObject<Issue> {
   private int startLine = 0;
   private DiagnosticSeverity severity = DiagnosticSeverity.Hint;
 
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public void setLocation(String location) {
-    this.location = location;
-  }
-
-  public String getLocation() {
-    return location;
-  }
-
-  public void setSeverity(DiagnosticSeverity severity) {
-    this.severity = severity;
-  }
-
-  public DiagnosticSeverity getSeverity() {
-    return severity;
-  }
-
   @Override
   public String toString() {
     return this.description + " " + this.location;
   }
 
-  public int getStartLine() {
-    return startLine;
-  }
-
-  public void setStartLine(int startLine) {
-    this.startLine = startLine;
-  }
 }
