3469d742c75e38cd621d49f025723e698e90fc80
Tymko Oleg <olegtymko@yandex.ru>
2020-11-27 22:44:21 +0700

    фикс конфига для BSL LS по умолчанию

--- src/main/java/com/github/otymko/phoenixbsl/logic/lsp/BSLConfiguration.java
+++ src/main/java/com/github/otymko/phoenixbsl/logic/lsp/BSLConfiguration.java
@@ -1,6 +1,5 @@
 package com.github.otymko.phoenixbsl.logic.lsp;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,33 +10,24 @@ import lombok.extern.slf4j.Slf4j;
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BSLConfiguration {
-
   private String language;
   private CodeLensOptions codeLens;
   private DiagnosticsOptions diagnostics;
-  private DocumentLinkOptions documentLink;
   private String traceLog;
   private String configurationRoot;
 
   @Data
   @JsonIgnoreProperties(ignoreUnknown = true)
   public static class CodeLensOptions {
-    private boolean showCognitiveComplexity = true;
-    private boolean showCyclomaticComplexity = true;
+    private boolean showCognitiveComplexity = false;
+    private boolean showCyclomaticComplexity = false;
   }
 
   @Data
   @JsonIgnoreProperties(ignoreUnknown = true)
   public static class DiagnosticsOptions {
-    @JsonIgnore
     private String computeTrigger = "onSave";
     private String skipSupport = "never";
-  }
-
-  @Data
-  @JsonIgnoreProperties(ignoreUnknown = true)
-  public class DocumentLinkOptions {
-    private boolean useDevSite = false;
   }
 
 }
