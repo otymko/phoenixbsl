fe70ca825731d37c498c0a5ebbba5c583bd0f14f
Tymko Oleg <olegtymko@yandex.ru>
2020-11-30 12:07:24 +0700

    equals от lombok

--- src/main/java/com/github/otymko/phoenixbsl/model/KeyboardShortcut.java
+++ src/main/java/com/github/otymko/phoenixbsl/model/KeyboardShortcut.java
@@ -1,11 +1,13 @@
 package com.github.otymko.phoenixbsl.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@EqualsAndHashCode
 public class KeyboardShortcut {
   private final boolean controlPressed;
   private final List<Integer> keyList;
@@ -19,24 +21,5 @@ public class KeyboardShortcut {
     this.controlPressed = controlPressed;
     keyList = new ArrayList<>();
     keyList.add(key);
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (obj == null) {
-      return false;
-    }
-
-    if (this.getClass() != obj.getClass()) {
-      return false;
-    }
-
-    var object = (KeyboardShortcut) obj;
-    return object.controlPressed == controlPressed && keyList.equals(object.keyList);
-  }
-
-  @Override
-  public int hashCode() {
-    return super.hashCode();
   }
 }
